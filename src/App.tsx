9f8ffedd5f5566b4735c11ccb9a249c07e62b0d8
Omar <guerrero9725@gmail.com>
2020-11-29 11:22:42 -0500

    feat: Set up project

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+import "./App.css";
+
+function App() {
+  return (
+    <div className="drawing-canvas-container">
+      <canvas id="canvas-id" width="200" height="200" color="#EBEDED"></canvas>
+    </div>
+  );
+}
+
+export default App;
